b0b0aa3311d1546cbd18c30a0437fed6a998af22
paschal <obipaschal65@gmail.com>
2023-03-04 23:51:24 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,12 @@ resource "aws_vpc" "main" {
     Name = "demovpc"
   }
 }
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "pubsubnet"
+  }
+}
