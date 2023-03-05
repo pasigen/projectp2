b8d419dc7551857a9f6677b3fc3e193d401a61b3
paschal <obipaschal65@gmail.com>
2023-03-05 00:41:42 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -36,4 +36,23 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "demogw"
   }
+
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+# route {
+#   ipv6_cidr_block        = "::/0"
+# egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+#  }
+
+  tags = {
+    Name = "demorouter"
+  }
 }
